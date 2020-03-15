e8ee163632a9aa92aa607e45e889baa2f5e1f415
saya-kira <slawnqiu@gmail.com>
2020-03-15 22:26:20 +0800

    使用 multipart 方法成功地建立了 http 檔案傳送伺服器！

--- src/http_client.py
+++ src/http_client.py
@@ -1,15 +1,40 @@
 import aiohttp
 import asyncio
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
 
-async def main():
+# async def fetch(session, url):
+#     async with session.get(url) as response:
+#         return await response.text()
+
+
+async def main(data):
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://127.0.0.1:8080/esugram')
-        print(html)
+        with aiohttp.MultipartWriter() as mpwriter:
+            part = mpwriter.append(data)
+            # part.set_content_disposition('binary')
+            part.headers[aiohttp.hdrs.CONTENT_TYPE] = 'binary'
+            mpwriter.append("the local size is " + str(len(data)))
+            async with session.post('http://localhost:8086/mp', data=mpwriter) as resp:
+                print('HTTP code is ', resp.status)
+                print(await resp.text())
+
+
+def read_file():  # 輸入檔案路徑，將檔案存入記憶體，返回位元組數組
+    filename = input('copy your file uri here: ')
+    filename = filename.replace('\"','',2)
+    try:
+        file_handle = open(filename, 'br')
+    except Exception as esu:  # 嘗試二進位打開檔案
+        print(esu)
+        return None
+    finally:
+        data_read = bytearray(file_handle.read())
+        file_handle.close()
+        return data_read
+
 
 if __name__ == '__main__':
+    data = read_file()
+    # print(len(data))
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(data))
--- src/http_server.py
+++ src/http_server.py
@@ -1,16 +1,36 @@
+import asyncio
+
+import aiohttp
 from aiohttp import web
 
 
-async def handle(request):
+async def calc_size(request):
+    reader = await request.multipart()
+    binary = None
+    while True:
+        part = await reader.next()
+        if part is None:
+            break
+        print(part.headers)
+        if part.headers[aiohttp.hdrs.CONTENT_TYPE] == 'binary':
+            print('file found!')
+            await asyncio.sleep(6)
+            binary = await part.read()
+    if binary is not None:
+        print(len(binary))
+
+    return web.Response(text='Parse success, the size is '+str(len(binary)))
+
+
+async def web_page(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
 
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
-
+app.add_routes([web.post('/mp', calc_size),
+                web.get('/{name}', web_page)])
 
 if __name__ == '__main__':
-    web.run_app(app)
+    web.run_app(app, port=8086)
