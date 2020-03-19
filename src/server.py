565458db38b8f12ff7862a42f6738af3bbaa0a3d
saya-kira <slawnqiu@gmail.com>
2020-03-19 19:14:47 +0800

    放棄對非同步等待用戶輸入的嘗試

--- src/server.py
+++ src/server.py
@@ -5,14 +5,15 @@ from math import sqrt
 from datetime import datetime
 
 
-def repeat(message):
-    time.sleep(3)
+async def repeat(message):
+    await asyncio.sleep(3)
     print(message)
+
 
 
 if __name__ == "__main__":
    # worker = asyncio.get_event_loop()
     while True:
-        user_input = input("please input something\n")
+        user_input = await async.input("please input something\n")
         threading.Thread(target=repeat, args=[user_input]).start()
         print('ok!')
