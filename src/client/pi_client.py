1b805df5e3c558d4dc98c73d1d041b301c3adb6b
saya-kira <slawnqiu@gmail.com>
2020-05-27 15:54:41 +0800

    整合了最新的代碼

--- src/client/pi_client.py
+++ src/client/pi_client.py
@@ -9,6 +9,14 @@ import sys
 import os
 import RPi.GPIO as GPIO
 import serial
+import logging
+
+
+# 日誌記錄級別
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M'
+                    )
 
 # 設定 GPIO
 GPIO.setwarnings(False)
@@ -31,7 +39,7 @@ bus = smbus.SMBus(1)
 
 # 控制用變數
 POWER_ON = False
-AUTO_BRIGHTNESS = True
+AUTO_BRIGHTNESS = False
 BRIGHTNESS = 4
 LED_GPIO_INDEX = [11, 16, 13, 15]
 TOUCH_SWITCH_GPIO = 40
@@ -70,7 +78,7 @@ async def led_control_task():
 
 # 觸摸板操作
 async def touch_switch_task():
-    global POWER_ON, AUTO_BRIGHTNESS
+    global POWER_ON, AUTO_BRIGHTNESS, BRIGHTNESS
     time_pressed = 0
     while True:
         if GPIO.input(TOUCH_SWITCH_GPIO) and time_pressed == 0:
@@ -80,16 +88,28 @@ async def touch_switch_task():
             time_pressed = 0
             if time_elapsed < TOUCH_LONG_THRESHOLD_MILISEC:
                 # 短按操作：開關電源
+                logging.info("Touch bar short pressed")
                 if POWER_ON:
                     POWER_ON = False
+                    BRIGHTNESS = 4
+                    logging.info("Power off")
                 else:
                     POWER_ON = True
+                    logging.info("Power on")
             else:
+                logging.info("Touch bar long pressed")
                 # 長按操作：開關自動亮度
-                if AUTO_BRIGHTNESS:
-                    AUTO_BRIGHTNESS = False
+                if POWER_ON:
+                    if AUTO_BRIGHTNESS:
+                        AUTO_BRIGHTNESS = False
+                        BRIGHTNESS = 4
+                        logging.info("Auto brightness off")
+                    else:
+                        AUTO_BRIGHTNESS = True
+                        logging.info("Auto brightness on")
+
                 else:
-                    AUTO_BRIGHTNESS = True
+                    logging.info("The Power is off, turn on the power first")
 
         await asyncio.sleep(0.03)
 
@@ -108,14 +128,37 @@ async def light_detector_task():
                 BRIGHTNESS = 3
             elif 800 <= num:
                 BRIGHTNESS = 4
+            logging.debug("Detected: " + str(num) + " Set: " + str(BRIGHTNESS))
 
         await asyncio.sleep(LIGHT_DETECT_INTERVAL_SEC)
+
+
+# 蓝牙通信操作
+async def bluetooth_task():
+    global BRIGHTNESS
+    while True:
+        if POWER_ON:
+            count = ser.inWaiting()
+            if count != 0:
+                recv = ser.read(count)
+                # TODO: 字符串怎麼讀出來？
+                logging.info("Bluetooth recv: " + str(recv))
+                if recv == b'31':
+                    BRIGHTNESS = 4
+                else:
+                    BRIGHTNESS = 0
+        else:
+            pass
+
+        await asyncio.sleep(0.05)
 
 
 def main():
     loop.create_task(led_control_task())
     loop.create_task(touch_switch_task())
     loop.create_task(light_detector_task())
+    loop.create_task(bluetooth_task())
+
     loop.run_forever()
 
 
